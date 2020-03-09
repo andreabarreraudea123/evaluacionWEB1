620b124880e548111506d2a20081db5baae739ad
andreabarreraudea123 <61071219+andreabarreraudea123@users.noreply.github.com>
2020-03-08 22:17:52 -0500

    ejercicio 9 evaluacionWEB1

---
+++ funciones.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function calcularEdad($nacimiento, $actual){
+    $edad=($actual-$nacimiento);
+    return($edad);
+}
+
+function calcularBiotipo($biotipo){
+
+    if($biotipo<1.70){
+        echo("Para una altura de ".$biotipo." su biotipo es bajo<br>");
+       
+    }  
+
+    elseif($biotipo>1.70 && $biotipo<1.80){
+        echo("Para una altura de ".$biotipo." su biotipo es medio<br>");
+    }
+    else
+    {
+        echo("Para una altura de ".$biotipo." su biotipo es alto<br>");
+    }
+
+
+
+
+
+}
+   
+
+
+
+    
+?>
